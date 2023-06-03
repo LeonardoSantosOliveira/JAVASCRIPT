c34bf9176e6233722186bff8513949abaaf1c961
LeonardoSantosOliveira <leoleosantos21@gmail.com>
2023-06-03 15:45:39 -0300

    f

---
+++ Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Vivem em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Voce é Estrangeiro!')
+}
---
+++ Aula12ex/ex015/script.js
@@ -0,0 +1,49 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = window.document.getElementById('txtano')
+    var res = window.document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked){
+            genero = 'Homem'
+            if (idade >= 0 && idade <10){
+                //criança
+                img.setAttribute('src', 'imagens/bebeh.jpg')
+            } else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'imagens/jovemh.jpg')
+            } else if (idade < 50){
+                //adulto
+                img.setAttribute('src', 'imagens/adultoh.jpg')
+            } else{
+                //idoso
+                img.setAttribute('src', 'imagens/idosoh.jpg')
+            }
+        } else if (fsex[1].checked){
+                genero = 'Mulher'
+                if (idade >= 0 && idade <10){
+                    //criança
+                    img.setAttribute('src', 'imagens/bebem.jpg')
+                } else if (idade < 21){
+                    //jovem
+                    img.setAttribute('src', 'imagens/jovemm.jpg')
+                } else if (idade < 50){
+                    //adulto
+                    img.setAttribute('src', 'imagens/adultom.jpg')
+                } else{
+                    //idoso
+                    img.setAttribute('src', 'imagens/idosom.jpg')
+                }
+            }
+            res.style.textAlign = 'center'
+            res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+            res.appendChild(img)
+        }
+}
