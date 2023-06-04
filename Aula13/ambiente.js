1814cd506a0331d3020cd0208e695889025dc4c4
LeonardoSantosOliveira <leoleosantos21@gmail.com>
2023-06-03 21:21:48 -0300

    2

---
+++ Aula13/ambiente.js
@@ -0,0 +1,24 @@
+/*
+var i = 1
+while (i <= 6){
+    console.log(`Passo ${i}`)
+    i++
+}
+*/
+i = 1
+do{
+    console.log(`Passo ${i}`)
+    i++
+} while (i <= 6)
+
+
+
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')*/
---
+++ Aula13/node_52f18dc746490.tmp.js
@@ -0,0 +1,23 @@
+var i = 1
+while (i <= 6){
+    console.log(`Passo ${i}`)
+    i++
+}
+
+i = 1
+
+do{
+    console.log(`Pasoo ${i}`)
+} while (i <= 6)
+
+
+
+
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')*/
