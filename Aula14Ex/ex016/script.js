b37282ea0c8545d920d02b947da0541bb672dd6f
LeonardoSantosOliveira <leoleosantos21@gmail.com>
2023-06-07 17:27:55 -0300

    asd

--- Aula14Ex/ex016/script.js
+++ Aula14Ex/ex016/script.js
@@ -1,5 +1,28 @@
-var agora = new Date()
-var hora = agora.getHours()
-var minutos = agora.getMinutes()
+var botao = window.document.querySelector('input#botao')
+botao.addEventListener('click', contar)
 
-window.alert(`Agora são exatamente: ${hora}:${minutos}`)
+function contar(){
+    var inicio = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.querySelector('div#res')
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        window.alert('Confira os dados e tente novamente.')
+    } else{
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        res.style.textAlign = 'center'
+        res.innerHTML = `Contando... <br>`
+        if (i < f){
+            for(var c = i; c <= f; c +=p){
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        } else if (i > f){
+            for(var c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
--- Aula14Ex/ex017/script.js
+++ Aula14Ex/ex017/script.js
@@ -2,18 +2,18 @@ var botao = document.querySelector('input#botao')
 botao.addEventListener('click', calcular)
 
 function calcular(){
-    var numero = document.querySelector('input#txtn')
+    var numero = window.document.querySelector('input#txtn')
     var tab = document.querySelector('select#seltab')
-    if (numero.value.length == 0){
-        window.alert('Digite um número válido')
+    if(numero.value.length == 0){
+        window.alert('Verifique o numero e tente novamente')
     } else{
         var n = Number(numero.value)
         tab.innerHTML = ''
         for (var i = 1; i <= 10; i++){
             var item = document.createElement('option')
-            item.text = `${i} x ${n} = ${i*n}`
+            item.text = `${i} x ${n} = ${n*i}`
             item.value = `tab${i}`
             tab.appendChild(item)
         }
     }
-}
+} 
